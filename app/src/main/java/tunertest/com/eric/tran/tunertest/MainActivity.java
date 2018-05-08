0662f173a7afa7cb96a538b4ade98ccdfdd97f09
Eric Tran <erictraaaan@gmail.com>
2018-05-08 11:27:50 -0400

    initial commit

---
+++ app/src/main/java/tunertest/com/eric/tran/tunertest/MainActivity.java
@@ -0,0 +1,13 @@
+package tunertest.com.eric.tran.tunertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
